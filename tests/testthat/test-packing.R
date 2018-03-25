35fb2f947b95859b05107ab620d6ccd840ff31ff
byapparov <byapparov@gmail.com>
2018-03-25 17:12:44 +0100

    Try to add packages to travis

--- tests/testthat/test-packing.R
+++ tests/testthat/test-packing.R
@@ -106,6 +106,8 @@ describe("mknapsack", {
 context("knapsask")
 describe("knapsask.lpsolve", {
   it("solves knapsack problem using lpsolve", {
+    library(lpSolve)
+    library(ROI.plugin.lpsolve)
     skip_if_not_installed("lpSolve")
     profit = c(12, 40, 20, 1)
     volume = c(40, 10, 30, 60)
@@ -135,7 +137,8 @@ describe("knapsask.glpk", {
   it("solves knapsack problem using glpk", {
     skip_if_not_installed("Rglpk")
     skip_if_not_installed("ROI.plugin.glpk")
-
+    library(Rglpk)
+    library(ROI.plugin.glpk)
     profit = c(12, 40, 20, 1)
     volume = c(40, 10, 30, 60)
     units = c(5L, 1L, 30L, 20L)
