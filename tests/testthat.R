2e20623dc0194b3644c8ad419c0007dcdacadd4c
Bulat Yapparov <byapparov@gmail.com>
2018-03-28 15:55:20 +0100

    Remove dependancy from tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,6 @@
 suppressPackageStartupMessages({
   library(testthat)
   library(data.table)
-  library(Rglpk)
-  library(rcbc)
-  library(ROI.plugin.glpk)
-  library(ROI.plugin.cbc)
-  library(ROI)
 })
 
 test_check("mknapsack")
